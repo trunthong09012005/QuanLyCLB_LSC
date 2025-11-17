fa292b8bafd089c0e67afa382e07503689ca0f8c
Tich-ne1625 <vuongtieulam10062005@gmail.com>
2025-11-18 03:55:07 +0700

    dky2

--- Controllers/RegisterController.cs
+++ Controllers/RegisterController.cs
@@ -67,7 +67,6 @@ namespace QuanLyCLB_LSC.Controllers
 
                 _context.ThanhViens.Add(thanhVien);
                 _context.SaveChanges(); // EF Core gán MaTv tự động
-                transaction.Commit();
 
                 // 4️⃣ Tạo TaiKhoan với MaTv
                 var taiKhoan = new TaiKhoan
